2129a72761f209e4c484d37c5adf4604e5c980c3
David Hofer <david.hofer@gmail.com>
2016-04-15 12:06:05 -0400

    Improved primality testing.

--- benchmark/test.cr
+++ benchmark/test.cr
@@ -1,5 +1,21 @@
+require "benchmark"
 require "big_int"
 require "../src/primes.cr"
 
+def timing_result(&block)
+  Benchmark.measure { yield }.real * 1000
+end
+
 # Cole's prime
-puts Primes.factorization(BigInt.new(2) ** 67 - 1)
+#puts Primes.factorization(BigInt.new(2) ** 67 - 1)
+
+big_number = BigInt.new(10) ** 8
+
+(1..100).each do |i|
+  test = big_number + i
+  if Primes.miller_rabin_prime?(test)
+    puts test
+    puts "Miller-Rabin: #{timing_result { puts Primes.miller_rabin_prime?(test) }}"
+    puts "naive: #{timing_result { puts Primes.naive_prime?(test) }}"
+  end
+end
--- src/primes.cr
+++ src/primes.cr
@@ -1,5 +1,15 @@
 class Primes
   def self.prime?(n : Int)
+    # NOTE(hofer): On my laptop, 10^8 is approximately where Miller-Rabin starts
+    # being faster.
+    if n < 100_000_000
+      naive_prime?(n)
+    else
+      miller_rabin_prime?(n)
+    end
+  end
+
+  def self.naive_prime?(n : Int)
     return false if n < 2
     return true if n == 2
 
@@ -7,6 +17,79 @@ class Primes
 
     return true
   end
+
+  def self.power(x : Int, n : Int, m : Int)
+    result = BigInt.new(1)
+    square = BigInt.new(x)
+
+    while n != 0
+      result = (result * square) % m if (n & 1) == 1
+      square = (square * square) % m
+      n >>= 1
+    end
+
+    return m.class.new(result)
+  end
+
+  def self.miller_rabin_prime?(n : Int)
+    return naive_prime?(n) if n < 100
+
+    n = BigInt.new(n)
+
+    samples = 0
+    temp_n = n
+
+    while temp_n != 0
+      samples += 1
+      temp_n >>= 1
+    end
+
+    samples = [20, samples].max
+
+    t = n - 1
+    s = 0
+
+    while t & 1 == 0
+      t >>= 1
+      s += 1
+    end
+
+    samples.times do
+      a = BigInt.new(2 + rand(n - 4))
+      x = power(a, t, n)
+
+      next if x == 1 || x == n - 1
+
+      (s - 1).times do
+        x = (x * x) % n
+        return false if x == 1
+        break if x == n - 1
+      end
+
+      return false if x != n - 1
+    end
+
+    return true
+  end
+
+  def self.fermat_prime?(n : Int)
+    counter = n
+
+    while counter != 0
+      if fermat_composite?((2..n - 2).to_a.sample, n)
+        return false
+      end
+      counter >>= 1
+    end
+
+    return true
+  end
+
+  def self.fermat_composite?(a : Int, n : Int)
+    power(a, n - 1, n) != n.class.new(1)
+  end
+
+  PRIMES = (2..10 ** 6).select { |n| n.prime? }
 
   # NOTE(hofer): Need this because Math.sqrt doesn't handle BigInts.
   def self.binary_search_sqrt(n)
@@ -32,8 +115,9 @@ class Primes
       n = n.abs
     end
 
-    if n == 2
-      return [convert_type([2, 1], n)]
+    if n.prime?
+      factors << convert_type([n, 1], n)
+      n = n.class.new(1)
     end
 
     current_number = n
